2b8243fd422d1c521858b8f85cfe4bdbb0faca5d
Doyoung.Kim <dyflower@nate.com>
2022-12-12 15:31:55 +0900

    :lipstick: Style : reset 속성 추가

--- src/styles/globalStyles.jsx
+++ src/styles/globalStyles.jsx
@@ -11,6 +11,24 @@ const GlobalStyle = createGlobalStyle`
 }
 
 ${reset}
+*{
+    box-sizing:border-box;
+}
+a {
+    color: inherit;
+    text-decoration: none;
+}
+input, button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+}
+ol, ul, li {
+    list-style: none;
+}
+img{
+    display: block;
+}
 
 :root {
     --font: 'Pretendard';
