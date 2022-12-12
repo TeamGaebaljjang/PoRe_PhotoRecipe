e7e66bfc040737ffd15552ec65dc8fe0adb8f9c9
Doyoung.Kim <dyflower@nate.com>
2022-12-12 13:49:21 +0900

    :lipstick: Design : color변수 설정
    컬러 변수 설정하였습니다.

--- src/styles/globalStyles.jsx
+++ src/styles/globalStyles.jsx
@@ -3,6 +3,16 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
 
+:root{
+    --black : #1C1C1C;
+    --light-red : #FF5757;
+    --deep-red : #FF1818;
+    --light-gray : #E8E8E8
+    --gray : #CDCDCD;
+    --deep-gray: #B4B4B4;
+    --super-gray : #767676
+}
+
 ${reset}
 
 .ir {
