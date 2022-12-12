68605aa4aa0513745e099dc45ebab1e6c00b4804
Heesu <heesu0303@naver.com>
2022-12-12 14:11:43 +0900

    🎨 Style : design 브랜치 생성
    - design 브랜치 생성
    - globalStyles.jsx 세미콜론 누락 수정

--- src/styles/globalStyles.jsx
+++ src/styles/globalStyles.jsx
@@ -7,10 +7,10 @@ const GlobalStyle = createGlobalStyle`
     --black : #1C1C1C;
     --light-red : #FF5757;
     --deep-red : #FF1818;
-    --light-gray : #E8E8E8
+    --light-gray : #E8E8E8;
     --gray : #CDCDCD;
     --deep-gray: #B4B4B4;
-    --super-gray : #767676
+    --super-gray : #767676;
 }
 
 ${reset}
