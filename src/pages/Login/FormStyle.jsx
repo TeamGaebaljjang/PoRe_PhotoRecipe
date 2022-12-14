7e062b8ee1d1a8e41f1ebb4a2cf49ff9942ccae5
minji.jo <mwinterj@daum.net>
2022-12-14 16:45:05 +0900

    💄 Design : Form 겹치는 style 컴포넌트 생성
    로그인, 회원가입, 프로필 설정, 수정 페이지에 겹치는 style 컴포넌트로
    분리했습니다.
    
    #15

---
+++ src/pages/Login/FormStyle.jsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+const FormStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  label {
+    font-size: 12px;
+  }
+
+  input {
+    font-size: 14px;
+    height: 32px;
+    border-bottom: 1px solid var(--gray);
+  }
+
+  input::placeholder {
+    font-size: 14px;
+    color: var(--light-gray);
+  }
+
+  input + label {
+    margin-top: 16px;
+  }
+
+  /* invalidSpan 추가시 스타일 */
+  span + label {
+    margin-top: 16px;
+  }
+`;
+
+export default FormStyle;
