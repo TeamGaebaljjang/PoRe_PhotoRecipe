78cb84d86e8b9e0c6a1bd265fc23c3b65fea1571
jjangi <tesla2030@naver.com>
2022-12-19 17:22:00 +0900

    ♻️  Refactor : react-kakao-map-sdk 라이브러리 사용으로 전환
    Kakao Maps API를 React에 맞게 포팅한 라이브러리, react-kakao-map-sdk로
    이후 구현해야할 여러 기능들도 해당 튜토리얼 참고해 구현해볼까함
    #29

--- src/pages/Map/KakaoMap.jsx
+++ src/pages/Map/KakaoMap.jsx
@@ -1,28 +1,13 @@
-import { useEffect } from 'react';
-
-const { kakao } = window;
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const KakaoMap = () => {
-  useEffect(() => {
-    const container = document.getElementById('map');
-    const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3,
-    };
-    const map = new kakao.maps.Map(container, options);
-
-    const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
-    const marker = new kakao.maps.Marker({
-      position: markerPosition,
-    });
-    marker.setMap(map);
-  }, []);
-
   return (
-    <div
-      id="map"
+    <Map
+      center={{ lat: 37.566826, lng: 126.9786567 }}
       style={{ width: '390px', height: 'calc(100vh - 108px)', zIndex: '0' }}
-    />
+    >
+      <MapMarker position={{ lat: 37.566826, lng: 126.9786567 }} />
+    </Map>
   );
 };
 
