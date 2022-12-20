aba2d908ae1ac9b6960d2c61d5b09068857ef84e
minji.jo <mwinterj@daum.net>
2022-12-20 13:16:47 +0900

    ✨ Feat :  팔로잉 피드 API get
    Post 컴포넌트를 사용하는 다른 페이지와 통일하는 것이 좋을 것 같아
    보류합니다.
    
    #33

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import HeaderFeed from '../../components/header/HeaderFeed';
 import Post from '../../components/card/Post';
 import NavBar from '../../components/navBar/NavBar';
@@ -12,13 +14,36 @@ export const Wrap = styled.div`
 `;
 
 const Feed = () => {
+  const [items, setItems] = useState('');
+  const URL = 'https://mandarin.api.weniv.co.kr';
+
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOTZjYjUyMTdhZTY2NjU4MWMzMzQ0MyIsImV4cCI6MTY3NjYxODE4MiwiaWF0IjoxNjcxNDM0MTgyfQ.dnnDfwwnekAWwoNEhCQiog5t8TaQ3msfRBRcNbdX3c8';
+
+  const getFeed = async () => {
+    try {
+      const res = await axios.get(`${URL}/post/feed/?limit=4`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-type': 'application/json',
+        },
+      });
+      setItems(res.data);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => getFeed, []);
+
   return (
     <Wrap>
       <HeaderFeed />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post items={items} />
+      <Post items={items} />
+      <Post items={items} />
+      <Post items={items} />
       <NavBar />
     </Wrap>
   );
