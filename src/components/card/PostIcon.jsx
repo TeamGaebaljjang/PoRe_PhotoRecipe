3e5635a5a5105eda7d3391e037796d4359dba151
minji.jo <mwinterj@daum.net>
2022-12-22 02:07:45 +0900

    ✨ Feat :  피드 좋아요/좋아요 취소 기능 구현
    - close #38
    
    #38

--- src/components/card/PostIcon.jsx
+++ src/components/card/PostIcon.jsx
@@ -6,44 +6,55 @@ import heartOff from '../../assets/icons/icon-heart-off.svg';
 import chat from '../../assets/icons/icon-chat.svg';
 
 const PostIcon = ({ posts }) => {
-  const [like, setLike] = useState(false);
-  // const [heartCount, setHeartCount] = useState('');
+  const [like, setLike] = useState('');
+  const [heartCount, setHeartCount] = useState('');
 
-  const handleHeart = () => {
-    if (like ? setLike(false) : setLike(true));
-  };
-
-  const handleSetLike = async () => {
+  const handleLike = async () => {
     const URL = 'https://mandarin.api.weniv.co.kr';
     const authToken = localStorage.getItem('token');
     const postId = posts?.id;
 
     try {
-      const res = await axios.post(`${URL}/post/${postId}/heart`, [], {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-          'Content-type': 'application/json',
-        },
-      });
-      console.log('좋아요 res : ', res);
-      console.log('좋아요 data : ', res.data);
-      console.log('postId : ', postId);
+      if (!like) {
+        const res = await axios.post(`${URL}/post/${postId}/heart`, [], {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+            'Content-type': 'application/json',
+          },
+        });
+        // console.log('좋아요 res : ', res);
+        // console.log('좋아요 data : ', res.data);
+        setHeartCount(res.data.post.heartCount);
+        setLike(true);
+      } else {
+        const res = await axios.delete(`${URL}/post/${postId}/unheart`, {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+            'Content-type': 'application/json',
+          },
+        });
+        // console.log('좋아요취소 res : ', res);
+        // console.log('좋아요취소 data : ', res.data);
+        setHeartCount(res.data.post.heartCount);
+        setLike(false);
+      }
+      // console.log('postId : ', postId);
     } catch (error) {
       console.log(error);
     }
   };
 
-  useEffect(() => handleSetLike, []);
+  useEffect(() => handleLike, []);
   useEffect(() => {
-    // setHeartCount(posts?.heartCount);
+    setHeartCount(heartCount);
     setLike(posts?.hearted);
   }, []);
 
   return (
     <Icon>
-      <IconCount onClick={handleHeart}>
+      <IconCount onClick={handleLike}>
         <IconImg src={like ? heartOn : heartOff} alt="좋아요" />
-        {posts.heartCount}
+        {heartCount}
       </IconCount>
 
       <IconCount>
