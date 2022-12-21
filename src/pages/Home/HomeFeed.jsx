afda1e5b41e175a34a31654720bd9612b7c2856a
heesu0303 <heesu0303@naver.com>
2022-12-22 04:16:52 +0900

    ✨ Feat : 홈 화면의 피드 포토존 사진 불러오기 구현
    - 최신순으로 구현
    
     #43

--- src/pages/Home/HomeFeed.jsx
+++ src/pages/Home/HomeFeed.jsx
@@ -1,16 +1,44 @@
-import { FeedList, Feed } from './homeStyle';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { FeedList, Feed, FeedImg } from './homeStyle';
 
 const HomeFeed = () => {
+  const [feedPost, setFeedPost] = useState([]);
+
+  // API 서버
+  const URL = 'https://mandarin.api.weniv.co.kr';
+
+  // 썸네일 리스트 API
+  const getFeed = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.get(`${URL}/product/PoRe_PhotoRecipe`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-type': 'application/json',
+        },
+      });
+
+      if (response) {
+        console.log(response.data);
+        setFeedPost(response.data.product);
+      }
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
   return (
     <FeedList>
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
-      <Feed />
+      {feedPost.map((item) => (
+        <Feed>
+          <FeedImg key={item.id} src={item.itemImage} alt="" />
+        </Feed>
+      ))}
     </FeedList>
   );
 };
