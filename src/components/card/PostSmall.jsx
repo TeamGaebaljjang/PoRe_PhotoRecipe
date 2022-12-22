8ca12e45b170741ad833ad3b37f5c2df6847cd49
Doyoung.Kim <dyflower@nate.com>
2022-12-22 10:52:28 +0900

    :lipstick: Design : 작은 피드 마진 수정
    프로필 피드에서 보여주는 작은 피드의 마진을 홈과 동일하게 수정

--- src/components/card/PostSmall.jsx
+++ src/components/card/PostSmall.jsx
@@ -2,13 +2,17 @@ import styled from 'styled-components';
 
 const Post = styled.img`
   display: inline;
-  width: 175px;
+  width: 170px;
   height: 190px;
   border: 1px solid var(--gray);
   border-radius: 10px;
-  margin-left: 13px;
-  margin-bottom: 10px;
+  margin: 5px;
+  margin-top: 0px;
   object-fit: cover;
+
+  :nth-child(2n-1) {
+    margin-left: 20px;
+  }
 `;
 
 const PostSmall = ({ posts }) => {
