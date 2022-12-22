283a49725035839b956b29096969d72a3cf2a0d0
Doyoung.Kim <dyflower@nate.com>
2022-12-23 03:27:44 +0900

    :sparkles: Feat : 로그아웃 모달 표시
    로그아웃 모달이 등장할 수 있도록 구현 #59

--- src/components/modal/ConfirmModal/LogoutModal.jsx
+++ src/components/modal/ConfirmModal/LogoutModal.jsx
@@ -1,11 +1,18 @@
 import { ConfirmModal, Btnwrap } from './confirmModalStyle';
 
-const LogoutModal = () => {
+const LogoutModal = ({ logoutHandler }) => {
   return (
     <ConfirmModal>
       <h3>로그아웃하시겠어요?</h3>
       <Btnwrap>
-        <button type="button">취소</button>
+        <button
+          type="button"
+          onClick={() => {
+            logoutHandler();
+          }}
+        >
+          취소
+        </button>
         <button type="button" className="caution-option">
           로그아웃
         </button>
--- src/components/modal/UnderModal/ProfileUnderModal.jsx
+++ src/components/modal/UnderModal/ProfileUnderModal.jsx
@@ -1,20 +1,38 @@
+import { useState } from 'react';
 import {
   UnderModalWrap,
   UnderModalCont,
   UnderModalCloseBtn,
 } from './underModalStyle';
+import LogoutModal from '../ConfirmModal/LogoutModal';
 
 const ProfileUnderModal = ({ modalHandler }) => {
+  const [logout, setLogout] = useState(false);
+  const logoutHandler = () => {
+    setLogout(!logout);
+  };
+
   return (
-    <UnderModalWrap>
-      <UnderModalCloseBtn onClick={modalHandler} />
-      <UnderModalCont>
-        <button type="button">설정 및 개인정보</button>
-      </UnderModalCont>
-      <UnderModalCont>
-        <button type="button">로그아웃</button>
-      </UnderModalCont>
-    </UnderModalWrap>
+    <>
+      {logout ? <LogoutModal logoutHandler={logoutHandler} /> : null}
+
+      <UnderModalWrap>
+        <UnderModalCloseBtn onClick={modalHandler} />
+        <UnderModalCont>
+          <button type="button">설정 및 개인정보</button>
+        </UnderModalCont>
+        <UnderModalCont>
+          <button
+            type="button"
+            onClick={() => {
+              setLogout(!logout);
+            }}
+          >
+            로그아웃
+          </button>
+        </UnderModalCont>
+      </UnderModalWrap>
+    </>
   );
 };
 
