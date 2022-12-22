04acb208ea6a605e0d8028a9259c360b0542d5e5
Doyoung.Kim <dyflower@nate.com>
2022-12-23 03:32:28 +0900

    :sparkles: Feat : 로그아웃 구현
    로그아웃 기능 구현 완료 #59

--- src/components/modal/ConfirmModal/LogoutModal.jsx
+++ src/components/modal/ConfirmModal/LogoutModal.jsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom';
 import { ConfirmModal, Btnwrap } from './confirmModalStyle';
 
 const LogoutModal = ({ logoutHandler }) => {
+  const navigate = useNavigate();
   return (
     <ConfirmModal>
       <h3>로그아웃하시겠어요?</h3>
@@ -13,7 +15,15 @@ const LogoutModal = ({ logoutHandler }) => {
         >
           취소
         </button>
-        <button type="button" className="caution-option">
+        <button
+          type="button"
+          className="caution-option"
+          onClick={() => {
+            localStorage.removeItem('accountname');
+            localStorage.removeItem('token');
+            navigate('/login');
+          }}
+        >
           로그아웃
         </button>
       </Btnwrap>
