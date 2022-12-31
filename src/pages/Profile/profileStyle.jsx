d83836ddb6316707c2558248bea13e29d66c054f
minji.jo <mwinterj@daum.net>
2022-12-31 20:18:31 +0900

    💄 Design : 프로필 화면 중간 사이즈 반응형 적용
    #134

--- src/pages/Profile/profileStyle.jsx
+++ src/pages/Profile/profileStyle.jsx
@@ -1,32 +1,70 @@
 import styled from 'styled-components';
 
 export const BackDrop = styled.div`
-  position: absolute;
-  left: 0;
+  position: fixed;
   top: 0;
+  left: calc(50vw - 195px);
   z-index: 20;
   width: 390px;
   height: 100vh;
-  background: rgba(0, 0, 0, 0.35);
+  background: rgba(0, 0, 0, 0.15);
   backdrop-filter: blur(2px);
+  @media all and (min-width: 720px) and (max-width: 940px) {
+    & {
+      position: fixed;
+      width: 100vw;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+  }
+  @media all and (min-width: 941px) {
+    & {
+      position: fixed;
+      width: 100vw;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+  }
 `;
 
 export const BackDropWhite = styled.div`
   position: fixed;
-  left: calc(50vw - 203px);
+  left: calc(50vw - 195px);
   top: 0;
   z-index: 20;
   width: 390px;
   min-height: 100vh;
   background: rgba(0, 0, 0, 0.15);
   backdrop-filter: blur(2px);
+  @media all and (min-width: 720px) and (max-width: 940px) {
+    & {
+      position: fixed;
+      width: 100vw;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+  }
 `;
 
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   height: 100vh;
   overflow: auto;
   padding-bottom: 60px;
   &::-webkit-scrollbar {
     display: none;
+  }
+  @media all and (min-width: 720px) and (max-width: 940px) {
+    & {
+      width: calc(100vw - 126px);
+      margin-left: 126px;
+    }
   }
 `;
