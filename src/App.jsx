3fd3e119819152bf95ebf50f6fc9340938bea94b
Doyoung.Kim <dyflower@nate.com>
2023-01-02 04:45:15 +0900

    :sparkles: Feat : 다크모드 기본 세팅
    버튼 동작 및 기본 세팅 #142

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 import Routes from './routes/Routes';
+import { darkMode, lightMode } from './theme/theme';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleMode = () => {
+    setIsDarkMode((current) => !current);
+  };
+
   return (
     <div>
-      <Routes />
+      <ThemeProvider theme={isDarkMode ? darkMode : lightMode}>
+        <Routes />
+        <button
+          type="button"
+          onClick={() => {
+            toggleMode();
+          }}
+          style={{
+            width: '30px',
+            height: '30px',
+            backgroundColor: 'red',
+            position: 'absolute',
+            top: '0',
+          }}
+        >
+          다크모드
+        </button>
+      </ThemeProvider>
     </div>
   );
 };
---
+++ src/theme/theme.jsx
@@ -0,0 +1,27 @@
+export const darkMode = {
+  color: '#ffffff',
+  bgColor: '#1C1C1C',
+};
+export const lightMode = {
+  color: '#1C1C1C',
+  bgColor: '#ffffff',
+};
+// :root {
+//     --black: #1C1C1C;
+//     --light-red: #FF5757;
+//     --deep-red: #FF1818;
+//     --light-gray: #E8E8E8;
+//     --gray: #CDCDCD;
+//     --deep-gray: #B4B4B4;
+//     --super-gray: #767676;
+//     --success-green: #50c927;
+// }
+
+// const Test = styled.p`
+//   color: ${(props) => props.theme.color};
+//   position: absolute;
+//   top: 200px;
+// `;
+
+// background-color: ${(props) => props.theme.bgColor};
+// color: ${(props) => props.theme.color};
