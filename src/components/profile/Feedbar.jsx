93c52720c1bc939d34b326293a1e8f60491ff12b
Doyoung.Kim <dyflower@nate.com>
2023-01-02 13:08:39 +0900

    :sparkles: Feat : 피드 리스트 버튼 다크모드
    피드 리스트 버튼에 다크 모드를 적용하였습니다. #142

--- src/components/profile/Feedbar.jsx
+++ src/components/profile/Feedbar.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+/* eslint-disable no-nested-ternary */
+import { useContext, useState } from 'react';
 import { Container, WhoseFeed, UserIdTitle, BtnWrap } from './feedbarStyle';
+import { ThemeContext } from '../../store/ThemeProvider';
 
 import AlbumOn from '../../assets/icons/icon-post-album-on.svg';
 import AlbumOff from '../../assets/icons/icon-post-album-off.svg';
+import AlbumOnDark from '../../assets/icons/icon-post-album-on-dark.svg';
+
 import ListOn from '../../assets/icons/icon-post-list-on.svg';
 import ListOff from '../../assets/icons/icon-post-list-off.svg';
+import ListOnDark from '../../assets/icons/icon-post-list-on-dark.svg';
 
 const FeedBar = ({ viewHandler, accountname }) => {
   const [click, setClick] = useState(true);
   const IconHandle = () => {
     setClick(!click);
   };
+  const { isDarkMode } = useContext(ThemeContext);
 
   return (
     <Container>
@@ -25,7 +31,10 @@ const FeedBar = ({ viewHandler, accountname }) => {
             viewHandler();
           }}
         >
-          <img src={click ? AlbumOff : AlbumOn} alt="앨범형으로 보기 아이콘" />
+          <img
+            src={click ? AlbumOff : isDarkMode ? AlbumOnDark : AlbumOn}
+            alt="앨범형으로 보기 아이콘"
+          />
         </button>
         <button
           type="button"
@@ -34,7 +43,10 @@ const FeedBar = ({ viewHandler, accountname }) => {
             viewHandler();
           }}
         >
-          <img src={click ? ListOn : ListOff} alt="리스트형으로 보기 아이콘" />
+          <img
+            src={click ? (isDarkMode ? ListOnDark : ListOn) : ListOff}
+            alt="리스트형으로 보기 아이콘"
+          />
         </button>
       </BtnWrap>
     </Container>
