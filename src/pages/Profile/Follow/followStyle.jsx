717b5c5cc221e208027b7ed901b060719e715609
minji.jo <mwinterj@daum.net>
2023-01-03 00:04:59 +0900

    💄 Design : 팔로우 리스트 화면에서 다크모드시 버튼 style 수정
    #142

--- src/pages/Profile/Follow/followStyle.jsx
+++ src/pages/Profile/Follow/followStyle.jsx
@@ -13,6 +13,11 @@ export const Wrap = styled.div`
       padding: 0 10%;
     }
   }
+  @media all and (min-width: 941px) {
+    & {
+      padding: 0 20%;
+    }
+  }
 `;
 // 제거예정
 export const Li = styled.li`
@@ -26,15 +31,15 @@ export const BtnFollow = styled.button`
   width: 55px;
   height: 28px;
   border-radius: 28px;
-  background-color: black;
-  color: white;
-  border: 1px solid black;
+  background-color: ${(props) => props.theme.color};
+  color: ${(props) => props.theme.bgColor};
+  border: 1px solid ${(props) => props.theme.color};
   font-size: 12px;
 
   ${(p) =>
     p.cancel &&
     css`
-      background-color: white;
-      color: black;
+      background-color: ${(props) => props.theme.bgColor};
+      color: ${(props) => props.theme.color};
     `}
 `;
