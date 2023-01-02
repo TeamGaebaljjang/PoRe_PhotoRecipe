62b5c9d607bdb401652083db7768d0ad9a5b630d
heesu0303 <heesu0303@naver.com>
2023-01-02 14:32:47 +0900

    💄 Design : header 다크모드 적용
    - 다크모드일 경우 header 아이콘들 적용
    
     #142

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,3 +1,12 @@
+import searchIcon from '../assets/icons/icon-search.svg';
+import moreIcon from '../assets/icons/icon-more-vertical.svg';
+import backIcon from '../assets/icons/icon-arrow-left.svg';
+import editIcon from '../assets/icons/icon-edit-post.svg';
+import searchIconDark from '../assets/icons/icon-search-white.svg';
+import moreIconDark from '../assets/icons/icon-more-vertical-white.svg';
+import backIconDark from '../assets/icons/icon-arrow-left-dark.svg';
+import editIconDark from '../assets/icons/icon-edit-post-white.svg';
+
 export const darkMode = {
   color: '#ffffff',
   bgColor: '#1C1C1C',
@@ -8,7 +17,14 @@ export const darkMode = {
   chatShadow: '1px 2px 5px -2px black',
   font: '#E8E8E8',
   realBlack: 'black',
+  scroll: 'var(--deep-gray)',
+  button: 'var(--super-gray)',
+  searchIcon: `url(${searchIconDark})`,
+  moreIcon: `url(${moreIconDark})`,
+  backIcon: `url(${backIconDark})`,
+  editIcon: `url(${editIconDark})`,
 };
+
 export const lightMode = {
   color: '#1C1C1C',
   bgColor: '#ffffff',
@@ -19,6 +35,12 @@ export const lightMode = {
   chatShadow: '1px 2px 5px -2px var(--deep-gray)',
   font: '#767676',
   realBlack: '#CDCDCD',
+  scroll: 'var(--gray)',
+  button: 'var(--gray)',
+  searchIcon: `url(${searchIcon})`,
+  moreIcon: `url(${moreIcon})`,
+  backIcon: `url(${backIcon})`,
+  editIcon: `url(${editIcon})`,
 };
 
 // :root {
