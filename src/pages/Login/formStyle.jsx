5bf5566abe826d5e6889df39909422740ad4a8c7
minji.jo <mwinterj@daum.net>
2023-01-02 15:04:01 +0900

    💄 Design : 로그인, 회원가입 화면 반응형 적용

--- src/pages/Login/formStyle.jsx
+++ src/pages/Login/formStyle.jsx
@@ -3,11 +3,9 @@ import styled from 'styled-components';
 const FormStyle = styled.div`
   display: flex;
   flex-direction: column;
-
   label {
     font-size: 12px;
   }
-
   input {
     font-size: 14px;
     height: 32px;
@@ -16,29 +14,30 @@ const FormStyle = styled.div`
       border-bottom: 1px solid var(--black);
     }
   }
-
   input::placeholder {
     font-size: 14px;
     color: var(--light-gray);
   }
-
   input:focus {
     border-bottom: 1px solid var(--black);
   }
-
   input + label {
     margin-top: 16px;
   }
-
   /* invalidSpan 추가시 스타일 */
   span + label {
     margin-top: 16px;
+  }
+  @media all and (min-width: 720px) {
+    & {
+      width: 50%;
+      margin: 0 auto;
+    }
   }
 `;
 
 const InvalidSpan = styled.span`
   display: block;
-  /* color: var(--light-red); */
   font-size: 12px;
   padding-top: 6px;
   &.success {
