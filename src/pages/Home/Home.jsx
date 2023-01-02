1e2e8cf43e1e3ab037ae43741c77670c34e88c80
Doyoung.Kim <dyflower@nate.com>
2023-01-02 09:09:03 +0900

    :sparkles: Feat : 모드 전환 버튼
    라이트, 다크모드 전환 버튼 #142

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,36 @@
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
 import { Wrap } from './homeStyle';
 import HomeCarousel from './HomeCarousel';
 // import HomeSpotBtn from './HomeSpotBtn';
 import HomeFeed from './HomeFeed';
 import NavBar from '../../components/navBar/NavBar';
 import BtnTop from '../../components/button/BtnTop';
+import { ThemeContext } from '../../store/ThemeProvider';
 
 const Home = () => {
   const parent = useRef();
+  const { isDarkMode, toggleMode } = useContext(ThemeContext);
+  console.log(isDarkMode);
 
   return (
     <Wrap ref={parent}>
+      <button
+        type="button"
+        onClick={() => {
+          toggleMode();
+        }}
+        style={{
+          width: '30px',
+          height: '30px',
+          backgroundColor: 'red',
+          position: 'absolute',
+          top: '16px',
+          left: '16px',
+          zIndex: '9999',
+        }}
+      >
+        다크모드
+      </button>
       <HomeCarousel />
       <HomeFeed />
       <NavBar />
