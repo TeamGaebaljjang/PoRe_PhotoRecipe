46c1a244d272d09c30facfbf4b159ba1e431d47a
heesu0303 <heesu0303@naver.com>
2023-01-02 12:01:36 +0900

    🔥 Remove : 주석 제거
    - 기존의 신고하기 주석 제거

--- src/components/modal/UnderModal/FeedEditUnder.jsx
+++ src/components/modal/UnderModal/FeedEditUnder.jsx
@@ -1,40 +1,19 @@
-import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import FeedDeleteModal from '../ConfirmModal/FeedDeleteModal';
-import ReportMiniModal from '../ConfirmModal/ReportMiniModal';
 import {
   UnderModalWrap,
   UnderModalCont,
   UnderModalCloseBtn,
-  CommentModalWrap,
 } from './underModalStyle';
 
-const FeedEditUnder = ({ posts, closeModal }) => {
+const FeedEditUnder = ({ posts }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const target = location.pathname;
   const [del, setDel] = useState(false);
-  const [report, setReport] = useState(false);
   const deleteHandler = () => {
     setDel(!del);
-  };
-
-  const ReportPost = async () => {
-    try {
-      const URL = 'https://mandarin.api.weniv.co.kr';
-      const authToken = localStorage.getItem('token');
-      const res = await axios.post(`${URL}/post/${posts.id}/report`, [], {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-          'Content-type': 'application/json',
-        },
-      });
-      console.log('ReportPost 응답 : ', res);
-      setReport(!report);
-    } catch (error) {
-      console.log(error);
-    }
   };
 
   return (
@@ -43,43 +22,37 @@ const FeedEditUnder = ({ posts, closeModal }) => {
         <FeedDeleteModal deleteHandler={deleteHandler} posts={posts} />
       ) : null}
 
-      {target === '/profile' ? (
-        <UnderModalWrap className="fd-under-modal">
-          <UnderModalCloseBtn />
-          <UnderModalCont>
-            <button
-              type="button"
-              onClick={() => {
-                setDel(!del);
-              }}
-            >
-              삭제
-            </button>
-          </UnderModalCont>
-          <UnderModalCont>
-            <button
-              type="button"
-              onClick={() => {
-                navigate('/profile/editfeed', {
-                  state: { posts: { posts } },
-                });
-              }}
-            >
-              수정
-            </button>
-          </UnderModalCont>
-        </UnderModalWrap>
-      ) : (
-        <>
-          <CommentModalWrap>
-            <UnderModalCloseBtn />
-            <UnderModalCont onClick={ReportPost}>
-              <button type="button">신고하기</button>
-            </UnderModalCont>
-          </CommentModalWrap>
-          {report ? <ReportMiniModal closeModal={closeModal} /> : null}
-        </>
-      )}
+      <UnderModalWrap
+        className={
+          target === '/feed/feeddetail'
+            ? 'fd-under-modal detail'
+            : 'fd-under-modal'
+        }
+      >
+        <UnderModalCloseBtn />
+        <UnderModalCont>
+          <button
+            type="button"
+            onClick={() => {
+              setDel(!del);
+            }}
+          >
+            삭제
+          </button>
+        </UnderModalCont>
+        <UnderModalCont>
+          <button
+            type="button"
+            onClick={() => {
+              navigate('/profile/editfeed', {
+                state: { posts: { posts } },
+              });
+            }}
+          >
+            수정
+          </button>
+        </UnderModalCont>
+      </UnderModalWrap>
     </>
   );
 };
