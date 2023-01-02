165144fefebc91edf5e98b83f73a9f0339750f04
Doyoung.Kim <dyflower@nate.com>
2023-01-02 12:43:35 +0900

    :sparkles: Feat : 모드 전환 버튼
    나이트, 다크 모드 전환 버튼 #142

---
+++ src/components/button/BtnNight.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const ModeBtn = styled.button`
+  width: 30px;
+  height: 30px;
+  background-color: red;
+  position: absolute;
+  top: 10px;
+  left: 16px;
+  z-index: 40;
+`;
+
+export default ModeBtn;
