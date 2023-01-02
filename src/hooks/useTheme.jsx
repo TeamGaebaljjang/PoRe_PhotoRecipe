a1aa98882a6cee7d2eb495156e8ad0d7f3699ee9
Doyoung.Kim <dyflower@nate.com>
2023-01-02 15:51:14 +0900

    :sparkles: Feat : 다크 모드 상태 저장
    새로고침시에도 다크, 라이트 모드의 상태가 저장 됩니다. #142

--- src/hooks/useTheme.jsx
+++ src/hooks/useTheme.jsx
@@ -1,10 +1,18 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useLayoutEffect, useState } from 'react';
 
 const useTheme = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const toggleMode = useCallback(() => {
     setIsDarkMode((current) => !current);
+    localStorage.setItem('mode', isDarkMode);
+  }, [isDarkMode]);
+
+  useLayoutEffect(() => {
+    const savedMode = localStorage.getItem('mode');
+    if (savedMode) {
+      setIsDarkMode(savedMode);
+    }
   }, []);
 
   return {
